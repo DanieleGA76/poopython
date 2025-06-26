1c07c63f0ab7da3cf7a83577586ad6ffb8ef18d7
= <ead@academico.local>
2025-06-26 17:36:33 -0300

    aula sobre interface

---
+++ 07_interface/main.py
@@ -0,0 +1,66 @@
+# importações
+import classes
+import os
+
+if __name__ == "__main__":
+    # instancia do objeto
+    usuario = classes.PessoaFisica("", "", "", "", "", "")
+    empresa = classes.PessoaJuridica("", "", "", "", "", "")
+
+    while True:
+        print("-"*60)
+        print("1 - Inserir dados do usuário")
+        print("2 - Inserir dados da empresa")
+        print("3 - Exibir dados")
+        print("4 - Sair do programa.")
+        opcao = input("Informe a opção desejada: ").strip()
+        os.system("cls" if os.name == "nt" else "clear")
+        match opcao:
+            case "1":
+                try:
+                    usuario.nome = input("Informe o nome do usuário: ").title().strip()
+                    usuario.cpf = input("Informe o CPF: ").strip()
+                    usuario.profissao = input("Informe a profissão: ").title().strip()
+                    usuario.email = input("Informe o e-mail: ").lower().strip()
+                    usuario.endereco = input("Informe o endereço: ").title().strip()
+                    usuario.telefone = input("Informe o telefone: ").strip()
+
+                    os.system("cls" if os.name == "nt" else "clear")
+
+                    print(f" Daddos de {usuario.nome} inseridas com sucesso!")
+                except Exception as e:
+                    print(f"Não foi possível inserir dados de usuário. {e}.")
+                finally:
+                    continue
+            case "2":
+                try:
+                    empresa.razao_social = input("Informe a razão social: ").strip().title()
+                    empresa.nome_fantasia = input("Informe nome da empresa: ").strip().title()
+                    empresa.cnpj = input("Informe CNPJ: ").strip()
+                    empresa.email = input("Informe e-mail: ").strip().lower()
+                    empresa.endereco = input("Informe o endereço da empresa: ").strip().title()
+                    empresa.telefone = input("Informe o telefone da empresa: ").strip()
+
+                    os.system("cls" if os.name == "nt" else "clear")
+
+                    print(f"Dados da emprsa {empresa.nome_fantasia} inseridos com sucesso!")
+                except Exception as e:
+                    print(f"Não foi possível inserir dados da empresa. {e}>")
+                finally:
+                    continue
+            case "3":
+                    try:
+                        print("\n DADOS DO USUÁRIO \n")
+                        usuario.exibir_dados()
+                        print("-"*60)
+                        print("\n DADOS DA EMPRESA \n")
+                        empresa.exibir_dados()
+                    except Exception as e:
+                        print(f"Não foi possível visualizar os dados. {e}.")
+                    finally:
+                        continue
+            case "4":
+                print("Programa encerrado.")
+                break
+            case _:
+                print("Opção Inválida.")
